3d738de1eb7bf017c9b8e9ab639e50c5f7c27720
Unknown <tonnius@gmail.com>
2018-02-25 23:28:58 +0200

    Forgot to commit all changes from previous commit
    Former-commit-id: c9ce1fa1048e960fa78b5f2792b26f1bc883398f

--- OpenVibe/ssvep_maka/runAll.sh
+++ OpenVibe/ssvep_maka/runAll.sh
@@ -2,23 +2,23 @@
 
 OPENVIBE_ROOT_PATH="$HOME/Git/openvibe/dist/extras-Release"
 MY_SCRIPT_PATH="$HOME/Git/magister_BCI/OpenVibe/ssvep_maka"
-INPUT_TRAINING_FILE_LOC="$MY_SCRIPT_PATH/signals/ssvep-record-[2018.02.13-15.22.10]-martin-8-5elect.ov"
-INPUT_TESTING_FILE_LOC="$MY_SCRIPT_PATH/signals/ssvep-record-[2018.02.13-15.31.38]-martin-test-no-zero-8-5-elect.ov"
+#INPUT_TRAINING_FILE_LOC="$MY_SCRIPT_PATH/signals/ssvep-record-[2018.02.13-15.22.10]-martin-8-5elect.ov"
+#INPUT_TESTING_FILE_LOC="$MY_SCRIPT_PATH/signals/ssvep-record-[2018.02.13-15.31.38]-martin-test-no-zero-8-5-elect.ov"
 
 declare -a trainingArr=("$MY_SCRIPT_PATH/signals/ssvep-record-[2018.02.13-15.22.10]-martin-8-5elect.ov" 
                 "$MY_SCRIPT_PATH/signals/ssvep-record-[2018.02.15-14.20.15]-teet.ov"
-                #"$MY_SCRIPT_PATH/signals/ssvep-record-[2018.01.10-15.32.49]-tonis.ov"                
+                "$MY_SCRIPT_PATH/signals/ssvep-record-[2018.01.10-15.32.49]-tonis.ov"                
                 )
 
 declare -a testingArr=("$MY_SCRIPT_PATH/signals/ssvep-record-[2018.02.13-15.31.38]-martin-test-no-zero-8-5-elect.ov" 
                 "$MY_SCRIPT_PATH/signals/ssvep-record-[2018.02.15-14.32.32]-teet-test-no-zero.ov"
-                #"$MY_SCRIPT_PATH/signals/ssvep-record-[2018.01.17-16.49.48]-tonis-test.ov"
+                "$MY_SCRIPT_PATH/signals/ssvep-record-[2018.01.17-16.49.48]-tonis-test.ov"
                 )
 
-PROC_EPOCH_DUR=0.25
+PROC_EPOCH_DUR=0.1
 PROC_EPOCH_INTER=0.1
-FREQ_TOLERANCE=0.25
-CHANNELS="1:5"
+FREQ_TOLERANCE=0.1
+CHANNELS="1:4"
 
 
 sudo "$OPENVIBE_ROOT_PATH/openvibe-designer.sh" --play-fast "$MY_SCRIPT_PATH/ssvep-configuration-auto.xml" --invisible \
@@ -30,14 +30,18 @@ wait
 
 arraylength=${#trainingArr[@]}
 
-for (( i=0; i<${arraylength}; i++ ));
+for ((i=0; i<${arraylength}; i++));
 do
 
 	sudo "$OPENVIBE_ROOT_PATH/openvibe-designer.sh" --play-fast "$MY_SCRIPT_PATH/CSP-training-auto.xml" --define inFile ${trainingArr[$i]} --invisible 
 	wait
 	sudo "$OPENVIBE_ROOT_PATH/openvibe-designer.sh" --play-fast "$MY_SCRIPT_PATH/classifier-training-auto.xml" --define inFile ${trainingArr[$i]} --invisible
 	wait
-	sudo "$OPENVIBE_ROOT_PATH/openvibe-designer.sh" --play-fast "$MY_SCRIPT_PATH/perf-measure-auto.mxs" --define inFile ${testingArr[$i]} --invisible
+	sudo "$OPENVIBE_ROOT_PATH/openvibe-designer.sh" --play-fast "$MY_SCRIPT_PATH/perf-measure-auto.mxs" --define inFile ${testingArr[$i]} --invisible \
+	--define epDur $PROC_EPOCH_DUR \
+    --define epInt $PROC_EPOCH_INTER \
+    --define freqTol $FREQ_TOLERANCE \
+	--define ch $CHANNELS
 	wait
 
 	echo $i " / " ${arraylength} " : " ${trainingArr[$i]}
--- OpenVibe/ssvep_maka/scripts/python-confusion-matrix.py
+++ OpenVibe/ssvep_maka/scripts/python-confusion-matrix.py
@@ -1,51 +1,81 @@
 import numpy as np
-#from sklearn.metrics import confusion_matrix
-#from pandas_ml import ConfusionMatrix
+import os
+# from pandas_ml import ConfusionMatrix
+import pickle
 
+
+# import shutil
 class MyOVBox(OVBox):
-	def __init__(self):
-		OVBox.__init__(self)
-		self.actualLabels = []
-		self.predictedLabels = []
+    def __init__(self):
+        OVBox.__init__(self)
+        self.actualLabels = []
+        self.predictedLabels = []
 
-	def initialize(self):
-		# nop
-		return
-		
-	def process(self):
-		stim = 0
-		for chunkIndex in range( len(self.input[0]) ):
-			chunk = self.input[0].pop()
-			if(type(chunk) == OVStimulationSet):
-				for stimIdx in range(len(chunk)):
-					stim=chunk.pop();
-					#print 'Received stim on input0', stim.identifier, 'stamped at', stim.date, 's'
-					self.actualLabels.append(stim.identifier)
+    def initialize(self):
+        # nop
+        return
 
-		for chunkIndex1 in range( len(self.input[1]) ):
-			chunk1 = self.input[1].pop()
-			if(type(chunk1) == OVStimulationSet):
-				for stimIdx1 in range(len(chunk1)):
-					stim1=chunk1.pop();
-					#if stim is not 0:
-						#if stim1.date > stim.date:
-					#print 'Received stim on input1', stim1.identifier, 'stamped at', stim1.date, 's'
-					self.predictedLabels.append(stim1.identifier)
+    def process(self):
+        stim = 0
+        for chunkIndex in range(len(self.input[0])):
+            chunk = self.input[0].pop()
+            if (type(chunk) == OVStimulationSet):
+                for stimIdx in range(len(chunk)):
+                    stim = chunk.pop()
+                    # print 'Received stim on input0', stim.identifier, 'stamped at', stim.date, 's'
+                    self.actualLabels.append(stim.identifier)
 
+        for chunkIndex1 in range(len(self.input[1])):
+            chunk1 = self.input[1].pop()
+            if (type(chunk1) == OVStimulationSet):
+                for stimIdx1 in range(len(chunk1)):
+                    stim1 = chunk1.pop()
+                    # if stim is not 0:
+                    # if stim1.date > stim.date:
+                    # print 'Received stim on input1', stim1.identifier, 'stamped at', stim1.date, 's'
+                    self.predictedLabels.append(stim1.identifier)
 
-			#else:
-			#	print 'Received chunk of type ', type(chunk), " looking for StimulationSet"
-		return
-		
-	def uninitialize(self):
-		with open('subj1', 'wb') as f:
-			np.savez(f, self.actualLabels, self.predictedLabels)
-		print 'File saved to '
-		#print 'actual labels array size: ', len(self.actualLabels), ' predictedLabels array size: ', len(self.predictedLabels)
-		#confusion_matrix = ConfusionMatrix(self.actualLabels, self.predictedLabels)
-		#print("Confusion matrix:\n%s" % confusion_matrix)
-		#confusion_matrix.print_stats()
+        # else:
+        #	print 'Received chunk of type ', type(chunk), " looking for StimulationSet"
+        return
 
-		
+    def uninitialize(self):
+        nrOfSubjects = 3
+        dirToWrite = '/home/tonnius/Git/magister_BCI/OpenVibe/ssvep_maka/data/'
+        dataDirFileNames = sorted(os.listdir(dirToWrite))
+        print dataDirFileNames
+        fileNrToWrite = dataDirFileNames[0]
+        exSettings = [self.setting['channels'],
+                      self.setting['Epoch Duration'],
+                      self.setting['Epoch Interval'],
+                      self.setting['Freq Tol']]
+
+        if int(fileNrToWrite) > nrOfSubjects:
+            fileNrToWrite = '1'
+
+        settingsFileName = ''
+        for setting in exSettings:
+            settingsFileName = settingsFileName + str(setting)[2:]
+
+        writeFile = dirToWrite + 'Ex' + settingsFileName + 'subject' + fileNrToWrite
+        labels = []
+
+        print 'Experiment setting: '.join([str(item) for item in exSettings])
+        labels.append(exSettings)
+        labels.append(self.actualLabels)
+        labels.append(self.predictedLabels)
+        pickle.dump(labels, open(writeFile, 'wb'))
+
+        os.rename(dirToWrite + dataDirFileNames[0], dirToWrite + str(int(fileNrToWrite) + 1))
+
+        # with open(dirToWrite, 'a') as f:
+        # np.savetxt(f, (self.actualLabels, self.predictedLabels))
+        print 'File saved to ' + writeFile
+        print 'actual labels array size: ', len(self.actualLabels), ' predictedLabels array size: ', len(
+            self.predictedLabels)
+    # confusion_matrix = ConfusionMatrix(self.actualLabels, self.predictedLabels)
+    # print("Confusion matrix:\n%s" % confusion_matrix)
+    # confusion_matrix.print_stats()
+
 
 box = MyOVBox()
