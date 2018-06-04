6640266868a4c9cc6b29a8b92562e2db1bdf3440
Unknown <tonnius@gmail.com>
2018-06-04 12:24:47 +0300

    Updated openvibe_to_ros_tcp and renamed some nodes in openvibe
     Comments and deleted unused functions

--- ROS/openvibe_to_ros_tcp/src/client_node.cpp
+++ ROS/openvibe_to_ros_tcp/src/client_node.cpp
@@ -1,3 +1,5 @@
+//Based on http://www.linuxhowtos.org/C_C++/socket.htm
+
 #include <ros/ros.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,27 +18,13 @@ void error(const char *msg) {
     exit(0);
 }
 
-class Listener {
-private:
-    char topic_message[256] = { 0 };
-public:
-    void callback(const std_msgs::String::ConstPtr& msg);
-};
-
-void Listener::callback(const std_msgs::String::ConstPtr& msg) {
-    memset(topic_message, 0, 256);
-    strcpy(topic_message, msg->data.c_str());
-    //ROS_INFO("I heard:[%s]", msg->data.c_str());
-}
-
 int main(int argc, char *argv[]) {
 	ros::init(argc, argv, "client_node");
 	ros::NodeHandle nh;
 
     ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
 
-    ros::Rate loop_rate(MESSAGE_FREQ); // set the rate as defined in the macro MESSAGE_FREQ
-	Listener listener;
+    ros::Rate loop_rate(MESSAGE_FREQ); // Set rate as defined in the macro MESSAGE_FREQ
 
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
@@ -48,18 +36,28 @@ int main(int argc, char *argv[]) {
        exit(0);
     }
     
-    portno = atoi(argv[2]);
+    // Open socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
 
+    portno = atoi(argv[2]);
     server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    // Configure socket parameters
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
+
     bcopy((char *)server->h_addr, 
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(portno);
+
+    // Connect to socket
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
 
@@ -67,18 +65,21 @@ int main(int argc, char *argv[]) {
     std::stringstream ss;
 
 	while(ros::ok()) {
-        ss.str(std::string()); //Clear contents of string stream
-        bzero(buffer,256);
-        n = read(sockfd,buffer,255);
-        if (n < 0) error("ERROR reading from socket");
+        ss.str(std::string()); // Clear contents of string stream
+        bzero(buffer, 256);
+        n = read(sockfd,buffer, 255); // Read msg from buffer
+        if (n < 0) 
+            error("ERROR reading from socket");
 
         ss << static_cast<int>(buffer[0]);
-        message.data = ss.str();
+        message.data = ss.str(); 
 
-        //ROS_INFO("%s", message.data.c_str());
-        chatter_pub.publish(message);
+        //ROS_INFO("I heard: %s", message.data.c_str());
+        chatter_pub.publish(message); // Publish msg to chatter
 
 	    ros::spinOnce();
 	}
+
+    close(sockfd);
 	return 0;
 }
